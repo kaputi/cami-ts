1281016c6fbd707025a54834de4b0f895dfce8e6
Eduardo Lonighi <edu_loni3@hotmail.com>
2020-05-11 12:05:06 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Product from './components/Product'
+import Store from './components/Store'
+import Default from './components/Default'
 
 const App = () => {
   return (
-    <div>
-      <p>App</p>
-    </div>
+    <>
+      <Navbar />
+      <Cart />
+      <Product />
+      <Store />
+      <Default />
+    </>
   )
 }
 
-export default App;
+export default App
---
+++ src/components/Default.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Default extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Default</h1>
+      </div>
+    );
+  }
+}
+
